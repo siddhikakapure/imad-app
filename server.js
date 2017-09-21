cf2ec3f1d6eab29a844bdb7a8b0d04a07bba9df1
siddhikakapure <sidflick@gmail.com>
2017-09-21 17:04:08 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -10,7 +10,7 @@ app.get('/', function (req, res) {
 });
 
 app.get('/article-one', function (req, res) {
-  res.sendFile('/index.html');
+  res.sendFile('index.html', { root: __dirname });
 });
 
 app.get('/article-two', function (req, res) {
